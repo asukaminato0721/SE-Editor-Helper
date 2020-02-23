6260ca011c863b5414cb96338592148224b0169a
HydrogenDeuterium <hydrogendeuterium1@gmail.com>
2020-02-23 16:26:34 +0800

    增加功能

---
+++ unit_fixer/unix_fixer_v0.1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020/2/23 14:52
+版权所有（c）<2020> 11598
+github:https://github.com/HydrogenDeuterium
+"""
+import pyperclip
+
+unitlist = ["kg", "sec"]
+
+i = pyperclip.paste()
+
+for unit in unitlist:
+    i = i.replace(r"\mathrm{" + unit + "}", unit)
+    i = i.replace(unit, r"\mathrm{" + unit + "}")
+print(i)
+
+pyperclip.copy(i)
